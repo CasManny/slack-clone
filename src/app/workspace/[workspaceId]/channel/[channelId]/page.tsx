5f3052cef2ed1ff57f6bc35e58e096e0deb25c02
CasManny <chukwusabastine2018@gmail.com>
2024-10-20 08:35:35 +0100

    format the rich text editor

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client";
+
+import { useGetChannel } from "@/features/channels/api/use-get-channel";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { Loader, TriangleAlert } from "lucide-react";
+import Header from "./header";
+import ChatInput from "./chat-input";
 
 const ChannelIdPage = () => {
-  return (
-    <div>ChannelIdPage</div>
-  )
-}
+  const channelId = useChannelId();
+  const { data: channel, isLoading: channelLoading } = useGetChannel({
+    id: channelId,
+  });
+  if (channelLoading) {
+    return (
+      <div className="flex h-full justify-center items-center flex-1">
+        <Loader className="text-muted-foreground animate-spin size-6" />
+      </div>
+    );
+  }
 
-export default ChannelIdPage
+  if (!channel) {
+    return (
+      <div className="flex h-full flex-col gap-y-2 justify-center items-center flex-1">
+            <TriangleAlert className="text-muted-foreground  size-6" />
+            <span className="text-sm text-muted-foreground">Channel not found</span>
+      </div>
+    );
+  }
+    return <div className="h-full flex-col flex">
+        <Header title={channel.name} />
+        <div className="flex-1" />
+        <ChatInput placeholder={`Message # ${channel.name}`} />
+
+  </div>;
+};
+
+export default ChannelIdPage;
---
+++ src/features/channels/api/use-update-channel.ts
@@ -0,0 +1,53 @@
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useCallback, useMemo, useState } from "react";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+type RequestType = { name: string, id: Id<'channels'> };
+type ResponseType = Id<"channels"> | null;
+
+type Options = {
+  onSuccess?: (data: ResponseType) => void;
+  onError?: (error: Error) => void;
+  onSettled?: () => void;
+  throwError?: boolean;
+};
+
+export const useUpdateChannel = () => {
+  const [status, setStatus] = useState<
+    "success" | "error" | "settled" | "pending" | null
+  >(null);
+  const [data, setData] = useState<ResponseType>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const isPending = useMemo(() => status === "pending", [status]);
+  const isSuccess = useMemo(() => status === "success", [status]);
+  const isError = useMemo(() => status === "error", [status]);
+  const isSettled = useMemo(() => status === "settled", [status]);
+
+  const mutation = useMutation(api.channels.update);
+  const mutate = useCallback(
+    async (values: RequestType, Options?: Options) => {
+      try {
+        setData(null);
+        setError(null);
+        setStatus("pending");
+        const response = await mutation(values);
+        Options?.onSuccess?.(response);
+        return response;
+      } catch (error) {
+          setStatus('error')
+        Options?.onError?.(error as Error);
+        if (Options?.throwError) {
+          throw Error;
+        }
+      } finally {
+        setStatus("settled");
+        Options?.onSettled?.();
+      }
+    },
+    [mutation]
+  );
+
+  return { mutate, error, isPending, isSettled, data, isSuccess, isError };
+};
